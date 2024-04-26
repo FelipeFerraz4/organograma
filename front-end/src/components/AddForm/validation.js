8a562b35e3b1beec776666daee75ae622bcc66b9
Felipe Ferraz <115377471+FelipeFerraz4@users.noreply.github.com>
2024-04-26 00:02:33 -0300

    creation of email, phone and university identifier validation

--- front-end/src/components/AddForm/validation.js
+++ front-end/src/components/AddForm/validation.js
@@ -1,6 +1,10 @@
 import alphabeticalValidation from '../../config/Validation/alphabeticalValidation';
 import dateValidation from '../../config/Validation/dateValidation';
-import emptyValidation from '../../config/Validation/emptyValidation';
+import emailValidation from '../../config/Validation/emailValidation';
+// import dateValidation from '../../config/Validation/dateValidation';
+import emptyValidation from '../../config/Validation/emptyvalidation';
+import phoneValidation from '../../config/Validation/phoneValidation';
+import universityIdentifierValidation from '../../config/Validation/universityIdentifierValidation';
 
 function additionEmployeeValidation(
     name,
@@ -24,8 +28,12 @@ function additionEmployeeValidation(
         return 'O campo Data de entrada na empresa não é válido, padrão Ano-dia-mês';
     } else if (!emptyValidation(email)) {
         return 'O campo E-mail está vazio';
+    } else if (!emailValidation(email)) {
+        return 'O campo E-mail não é válido';
     } else if (!emptyValidation(phone)) {
         return 'O campo Telefone está vazio';
+    } else if (!phoneValidation(phone)) {
+        return 'O campo Telefone não é válido';
     } else if (!emptyValidation(cpf)) {
         return 'O campo CPF está vazio';
     } else if (!emptyValidation(birthday)) {
@@ -34,6 +42,8 @@ function additionEmployeeValidation(
         return 'O campo Data de aniversário não é válido, padrão Ano-dia-mês';
     } else if (!emptyValidation(universityIdentifier)) {
         return 'O campo Identificação da universidade está vazio';
+    } else if (!universityIdentifierValidation(universityIdentifier)) {
+        return 'O campo Identificação da universidade não é válido';
     } else if (!emptyValidation(nationality)) {
         return 'O campo Nacionalidade está vazio';
     } else if (!alphabeticalValidation(nationality)) {
